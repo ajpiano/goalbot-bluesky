92d04097c78bfc76c0dcd9aedc4cb920ca9acc4f
adam j. sontag <ajpiano@ajpiano.com>
2024-11-15 21:16:35 -0500

    Add keyv-file for minimalistic caching

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ import 'dotenv/config';
 import axios from 'axios';
 import { RapidApi } from 'rapidapi-node-sdk';
 import Keyv from 'keyv';
+import { KeyvFile } from 'keyv-file';
 //import data from "./testlivefixtures.json" assert { type: "json" };
 
+const cache = new Keyv({store: new KeyvFile(), ttl: 60 * 1000, namespace: 'goalbot'});
 
 const rapidApi = new RapidApi({
     rapidApiHost: process.env.RAPIDAPI_HOST,
     rapidApiKey: process.env.RAPIDAPI_KEY,
     baseUrl: process.env.RAPIDAPI_BASE_URL,
     logger: console,
-    cache: new Keyv()
+    cache: cache
 });
 
-
+for await (const [key, value] of cache.iterator()) {
+  console.log(key);
+};
 
 const options = {
   method: 'GET',
@@ -35,10 +39,10 @@ await bot.login({
 //const text = `Get ready for the big one! It's ${nextMatch.teams.home.name} vs ${nextMatch.teams.away.name} on ${nextMatch.fixture.date} in the ${nextMatch.league.round}! Wowie!`
 
 
-console.log(data);
+//console.log(data);
 var aMatch = data.response.response[Math.floor(Math.random() * data.response.response.length)];
 
-console.log(aMatch.fixture);
+//console.log(aMatch.fixture);
 const text = `Coming to you live from ${aMatch.fixture.venue.name}, we have ${aMatch.league.name} action in ${aMatch.fixture.venue.city}, it's ${aMatch.teams.home.name} vs ${aMatch.teams.away.name}! It's currently the ${aMatch.fixture.status.elapsed}' of the match with the score ${aMatch.goals.home} - ${aMatch.goals.away}!`;
 
 console.log(text);
